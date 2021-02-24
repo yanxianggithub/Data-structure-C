dc183de6b93bd47d93e098344ef9476f7c9e7406
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-02-24 19:10:51 +0800

    0

---
+++ 00-other/leapyear.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int isleapyear(int year) {
+	int isleapyear = 0;
+	if(year % 4 == 0) {
+		if(year % 100 == 0) {
+			if(year % 400 == 0) {
+				isleapyear = 1;
+			}
+		}
+		isleapyear = 1;
+	}
+	return isleapyear;
+}
+
+int main() {
+	int i;
+	printf("input year:");
+	scanf("%d",&i);
+	if(isleapyear(i) == 1)
+		printf("%d:isleapyear.",i);
+	else printf("%d:not leapyear.",i);
+	return 0;
+}
