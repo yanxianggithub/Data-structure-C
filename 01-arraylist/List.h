9c4252d401989273cfcdcd06ce7285318442a850
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-03-13 12:43:32 +0800

    0

---
+++ 01-arraylist/List.h
@@ -0,0 +1,14 @@
+#define  MAXSIZE  100
+      typedef  struct  node { 
+              int  data[MAXSIZE];
+              int  length;
+      }SeqList, *PSeqList;
+
+PSeqList Init_SeqList(void);
+void Destroy_SeqList(PSeqList *PL);
+int Length_SeqList (PSeqList PL);
+int Location_SeqList (PSeqList PL,int x);
+int Insert_SeqList(PSeqList PL, int i,int x);
+int Delete_SeqList(PSeqList PL，int i);
+void Reverse_SeqList(PSeqList PL);
+int merge_SeqList(PSeqList A, PSeqList B, PSeqList C);
