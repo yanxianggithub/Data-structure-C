9f2ff03d62d968b30ac8adc6e695e8632c3a7d9b
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-05-15 21:31:41 +0800

    0

--- 98-experiment/migong.c
+++ 98-experiment/migong.c
@@ -4,24 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define row 10
 #define col 10
-typedef struct {
-    int cent,up,down,left,right;
-}Point;
+struct Point {
+    int i, j, up, down, left, right;
+};
 int a[row][col] = {0};
-static int in, inn, out, outt;
-
+static int n = 0, in, inn, out, outt;
 void Edge();
-
-void Display();
-
-void Random();
-
 void Path();
-
 void Input();
+void Random();
+void Display();
+void Display_o();
 
 int main()
 {
@@ -30,7 +25,7 @@ int main()
     Random();
     Path();
     Display();
-
+    Display_o();
     return 0;
 }
 
@@ -48,6 +43,18 @@ void Edge()
     }
 }
 
+void Random()
+{
+    int i, j;
+    for (int k = 0; k < row * 4; k++)
+    {
+        i = rand() % row;
+        j = rand() % col;
+        if (a[i][j] == 0)
+            a[i][j] = -1;
+    }
+}
+
 void Display()
 {
     int i, j;
@@ -61,55 +68,24 @@ void Display()
     }
 }
 
-void Random()
+void Display_o()
 {
     int i, j;
-    for (int k = 0; k < row * 4; k++)
+    printf("-------------------------------\n");
+    for (i = 0; i < col; i++)
     {
-        i = rand() % row;
-        j = rand() % col;
-        if (a[i][j] == 0)
-            a[i][j] = -1;
-    }
-}
-
-void Path()
-{
-    int b[row][col] = {0};
-    memcpy(b, a, sizeof(int) * row * col);
-    int x,y;
-    x=in;y=inn;
-    Point point;
-    point.cent=b[x][y];
-    point.up=b[x][y-1];
-    point.down=b[x][y+1];
-    point.left=b[x-1][y];
-    point.right=b[x+1][y];
-    while (1)
-    {
-        switch (rand() %4)
+        for (j = 0; j < row; j++)
         {
-            case 1:if(point.up==0)
-                {
-                    point.cent=point.up;
-                }else break;
-            case 2:if(point.down==0)
-                {
-                    point.cent=point.down;
-                } else break;
-            case 3:if(point.left==0)
-                {
-                    point.cent=point.left;
-                }else break;
-            case 4:if(point.right==0)
-                {
-                    point.cent=point.right;
-                } else break;
-            default: point.cent=-1;
+            if(a[i][j]==0||a[i][j]==-1)
+            {
+                printf("   ");
+            }else
+            {
+                printf("%3d", a[i][j]);
+            }
         }
-        if(point.up==-3||point.down==-3||point.right==-3||point.left==-3) return;
+        printf("\n");
     }
-
 }
 
 void Input()
@@ -134,5 +110,66 @@ void Input()
     {
         printf("出口不合理！");
         goto begin;
+    }
+}
+
+void Path()
+{
+    struct Point point[30];
+    int i = in, j = inn;
+    int b[row][col] = {0};
+    memcpy(b, a, sizeof(int) * row * col);
+    goto fetch;
+    while (1)
+    {
+        if (point[n].down == 0)
+        {
+            i++;
+            n++;
+            goto fetch;
+        } else if (point[n].up == 0)
+        {
+            i--;
+            n++;
+            goto fetch;
+        } else if (point[n].right == 0)
+        {
+            j++;
+            n++;
+            goto fetch;
+        } else if (point[n].left == 0)
+        {
+            j--;
+            n++;
+            goto fetch;
+        } else
+        {
+            b[point[n].i][point[n].j] = -1;
+            n--;
+            i = point[n].i;
+            j = point[n].j;
+        }
+        fetch:
+        {
+            point[n].i = i;
+            point[n].j = j;
+            point[n].up = b[i - 1][j];
+            point[n].down = b[i + 1][j];
+            point[n].left = b[i][j - 1];
+            point[n].right = b[i][j + 1];
+            b[i][j] = n + 1;
+        };
+        if (point[n].up == -3 || point[n].down == -3 || point[n].left == -3 || point[n].right == -3)break;
+    }
+    printf("-------------------------------\n");
+    for (int k = 0; k < col; k++)
+    {
+        for (int l = 0; l < row; l++)
+        {
+            if (b[k][l] > 1)
+            {
+                a[k][l] = b[k][l] - 1;
+            }
+        }
     }
 }
