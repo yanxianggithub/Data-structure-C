5e607b0e968efb062899dbcb707b3d4993b84bc6
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2021-08-07 14:21:13 +0800

    0

--- 98-experiment/sort.h
+++ 98-experiment/sort.h
@@ -1,5 +1,5 @@
 //
-// Created by asus on 2021/6/6.
+// Created by Yanxiang on 2021/6/6.
 //
 #ifndef DATA_STRUCTURE_C_SORTING_H
 #define DATA_STRUCTURE_C_SORTING_H
@@ -8,20 +8,21 @@
 #include <string.h>
 #include <time.h>
 #define SIZE 78
-struct Student{
+struct Student
+{
     int Number;
     char Name[8];
     char Sex[4];
     int Class;
     int Grade;
-}stu[SIZE];
+} stu[SIZE];
 void ReadFile(char *FileName);
-void WriteFile(char *FileName,struct Student *s);
-void HeapAdjust(struct Student *s,int n,int m);
+void WriteFile(char *FileName, struct Student *s);
+void HeapAdjust(struct Student *s, int n, int m);
 void HeapSort(struct Student *s);
-void Divide(struct Student *s1,struct Student *s2);
+void Divide(struct Student *s1, struct Student *s2);
 void BubbleSort(struct Student *s);
 void Merge(struct Student r[], struct Student rf[], int u, int v, int t);
-void MSort(struct Student p[],struct Student p1[],int n,int t);
+void MSort(struct Student p[], struct Student p1[], int n, int t);
 void MergeSort(struct Student *s);
 #endif //DATA_STRUCTURE_C_SORTING_H
