bfb13748e1d571231f6c164e75ebf98f14186758
yanxianggithub <1023305862@qq.com>
2022-03-01 23:10:45 +0800

    test

---
+++ build/CMakeFiles/CmakeList.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CmakeList.dir/main.cpp.obj"
+  "CMakeFiles/CmakeList.dir/main.cpp.obj.d"
+  "CmakeList.exe"
+  "CmakeList.exe.manifest"
+  "CmakeList.pdb"
+  "libCmakeList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CmakeList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CmakeList.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CmakeList.
+# This may be replaced when dependencies are built.
