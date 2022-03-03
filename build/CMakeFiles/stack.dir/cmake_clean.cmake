f8ae2772edd4b210785ce492de3b1c51d9904569
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2022-03-03 11:59:56 +0800

    0

---
+++ build/CMakeFiles/stack.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stack.dir/03-stack/Stack_Array.c.obj"
+  "CMakeFiles/stack.dir/03-stack/Stack_Array.c.obj.d"
+  "libstack.dll.a"
+  "stack.exe"
+  "stack.exe.manifest"
+  "stack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/stack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
