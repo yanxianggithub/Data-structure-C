b5ba0aa2ecd7cb69f0ac2151210c1803f5704fd4
yanxianggithub <65547481+yanxianggithub@users.noreply.github.com>
2022-03-13 23:57:55 +0800

    0

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/asus/Documents/GitHub/Data-structure-C
-# Build directory: C:/Users/asus/Documents/GitHub/Data-structure-C/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
